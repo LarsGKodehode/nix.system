e0596a100c8f7247c3b93b30caa5bc215f8eb32a
zabronax <zabronax.gmail.com>
2025-02-16 13:57:49 +0000

    add wsl derivation

---
+++ host/luna/default.nix
@@ -0,0 +1,61 @@
+{
+  inputs,
+  globals,
+  overlays,
+}:
+
+let
+  system = "x86_64-linux";
+in
+inputs.nixpkgs.lib.nixosSystem {
+  inherit system;
+
+  modules = [
+    ../../modules/shared
+    (
+      globals
+      // rec {
+        homePath = "/home/${globals.user}";
+      }
+    )
+    inputs.wsl.nixosModules.wsl
+    inputs.home-manager.nixosModules.home-manager
+    {
+      # Pin state version
+      system.stateVersion = "25.05";
+
+      # Replace config with our directory, as it's sourced on every launch
+      system.activationScripts.configDir.text = ''
+        rm -rf /etc/nixos
+        ln --symbolic --no-dereference --force /home/zab/system /etc/nixos
+      '';
+
+      # Configuration
+      networking.hostName = "luna";
+
+      theme = {
+        colors = (import ../../colorscheme/gruvbox-dark).dark;
+        dark = true;
+      };
+
+      # WSL specific
+      wsl = {
+        enable = true;
+        defaultUser = globals.user;
+        wslConf.network.generateResolveConf = true; # Turn off if it breaks VPN
+        interop.includePath = false; #Including Windows PATH will slow down other systems, filesystem cross talk
+      };
+
+      # Applications
+      alacritty.enable = false;
+
+      # Programming Toolchains
+      toolchain.nix.enable = true;
+      toolchain.misc-tooling.enable = false;
+      toolchain.kubernetes.enable = false;
+      toolchain.infrastructure-as-code.enable = false;
+      toolchain.rust.enable = false;
+      toolchain.dotnet.enable = false;
+    }
+  ];
+}
--- modules/shared/applications/default.nix
+++ modules/shared/applications/default.nix
@@ -4,6 +4,6 @@
   imports = [
     ./alacritty.nix
     ./1password.nix
-    ./dnsmasq.nix
+    # ./dnsmasq.nix
   ];
 }
