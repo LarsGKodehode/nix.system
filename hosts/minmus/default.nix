36d5abde78771d78f356a34f59d3478589649be2
zabronax <zabronax@gmail.com>
2026-01-29 20:43:39 +0100

    Standardize minmus host configuration
    - Add module order comment for clarity
    - Add unfree predicate for 1password-cli (matches lupus)
    - Add system.primaryUser with TODO comment
    - Remove alacritty.enable (WezTerm configured in darwin modules)
    - Standardize structure and comments to match lupus
    - Remove nix toolchain (not needed on minmus)

--- hosts/minmus/default.nix
+++ hosts/minmus/default.nix
@@ -7,19 +7,45 @@
 inputs.darwin.lib.darwinSystem {
   system = "aarch64-darwin";
 
+  # The modules list is executed in order
   modules = [
+    # Unfree predicate for system packages
+    # Needs to happen before any nixpkgs consumers are called
+    (
+      { config, pkgs, ... }:
+      {
+        nixpkgs.config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (pkgs.lib.getName pkg) [
+            "1password-cli"
+          ];
+      }
+    )
+
+    # Shared modules
     ../../modules/shared
     ../../modules/darwin
+
+    # Home Manager
+    inputs.home-manager.darwinModules.home-manager
+
+    # Host-Global values
     (
       globals
       // {
         homePath = "/Users/${globals.user}";
       }
     )
-    inputs.home-manager.darwinModules.home-manager
+
+    # Host specific configuration
     {
       # Configuration
       networking.hostName = "minmus";
+
+      # Temporary fix for darwin-nix
+      # TODO: Remove this once the "The Plan" is implemented (switching to system wide activation)
+      # https://github.com/nix-darwin/nix-darwin/issues/1452
+      system.primaryUser = globals.user;
 
       theme = {
         colors = (import ../../colorscheme/gruvbox-dark).dark;
@@ -27,11 +53,7 @@ inputs.darwin.lib.darwinSystem {
       };
 
       # Applications
-      alacritty.enable = true;
-
-      # Language Toolchains
-
-      # Services
+      _1password-cli.enable = true;
     }
   ];
 }
