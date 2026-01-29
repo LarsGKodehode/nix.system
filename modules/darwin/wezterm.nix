a6f00c8c48b167044f46195e5d093bca1bbcc5d0
zabronax <zabronax@gmail.com>
2026-01-29 20:27:53 +0100

    Configure WezTerm to use theme color scheme
    - Use config.theme.colors instead of hardcoded values
    - Matches pattern used in Alacritty and other modules
    - Ensures consistency across the system

--- modules/darwin/wezterm.nix
+++ modules/darwin/wezterm.nix
@@ -21,6 +21,34 @@
         })
         config.font_size = 12.0
 
+        -- Gruvbox Dark color scheme using theme source
+        config.colors = {
+          background = '${config.theme.colors.base00}',
+          foreground = '${config.theme.colors.base05}',
+          cursor_bg = '${config.theme.colors.base05}',
+          cursor_fg = '${config.theme.colors.base00}',
+          ansi = {
+            '${config.theme.colors.base00}', -- black
+            '${config.theme.colors.base08}', -- red
+            '${config.theme.colors.base0B}', -- green
+            '${config.theme.colors.base0A}', -- yellow
+            '${config.theme.colors.base0D}', -- blue
+            '${config.theme.colors.base0E}', -- magenta
+            '${config.theme.colors.base0C}', -- cyan
+            '${config.theme.colors.base05}', -- white
+          },
+          brights = {
+            '${config.theme.colors.base03}', -- bright black
+            '${config.theme.colors.base09}', -- bright red
+            '${config.theme.colors.base0B}', -- bright green
+            '${config.theme.colors.base0A}', -- bright yellow
+            '${config.theme.colors.base0D}', -- bright blue
+            '${config.theme.colors.base0E}', -- bright magenta
+            '${config.theme.colors.base0C}', -- bright cyan
+            '${config.theme.colors.base07}', -- bright white
+          },
+        }
+
         -- Launch fish shell by default
         config.default_prog = { '${pkgs.fish}/bin/fish', '-l' }
 
