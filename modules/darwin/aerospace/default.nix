7c90852bfecd43aed69c9b72f5c3ea2891499331
zabronax <zabronax@gmail.com>
2026-01-29 20:33:15 +0100

    Update aerospace configuration with launchd settings
    - Enable launchd integration
    - Set keepAlive to ensure aerospace stays running

--- modules/darwin/aerospace/default.nix
+++ modules/darwin/aerospace/default.nix
@@ -9,6 +9,10 @@
   home-manager.users.${config.user} = lib.mkIf pkgs.stdenv.isDarwin {
     programs.aerospace = {
       enable = true;
+      launchd = {
+        enable = true;
+        keepAlive = true;
+      };
       settings = builtins.fromTOML (builtins.readFile ./config.toml);
     };
   };
